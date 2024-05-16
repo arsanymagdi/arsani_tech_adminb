0c5377330f48cac2becfbff972618b9b32ae7c85
Arsany Magdy <109736550+arsanymagdi@users.noreply.github.com>
2024-05-16 16:40:28 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,71 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_webview/WebviewExample.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+class WebviewExample extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState()=>WebviewExampleState();
+
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class WebviewExampleState extends State<WebviewExample> {
+  late WebViewController controller;
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'arsani-tech'),
-    );
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://arsani-tech-adminboard-684yc3.flutterflow.app/'));
+
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Webview_flutter"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-              ElevatedButton(onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (_)=>WebviewExample()));
-              }, child: Text("admin"))
+    return WillPopScope(
+      onWillPop: () async{
+        if(await controller.canGoBack()){
+          controller.goBack();
+          return false;
+        }else{
+          return true;
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("admin"),
+          actions: [
+            IconButton(onPressed: ()async{
+              if(await controller.canGoBack()){
+                controller.goBack();
+              }
+
+            }, icon: Icon(Icons.arrow_left)),
+            IconButton(onPressed: ()async{
+              if(await controller.canGoForward()){
+              controller.goForward();
+              }
+            }, icon: Icon(Icons.arrow_right))
           ],
+        ),
+        body: WebViewWidget(controller: controller,
         ),
       ),
     );
